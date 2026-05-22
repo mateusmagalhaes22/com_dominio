027834669ce72a28b3d3bebeadab97e7f0591b44
mateus <mateus.magalhaes@hotmail.com.br>
2026-05-22 11:12:26 -0300

    cors origins log removed. app.enableCors before NestFactory.create()

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,22 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+
   // Configuração do CORS
   const corsOrigins = process.env.CORS_ORIGINS 
     ? process.env.CORS_ORIGINS.split(',').map(origin => origin.trim())
     : ['http://localhost:3000'];
 
-  console.log('CORS_ORIGINS env:', process.env.CORS_ORIGINS);
-
   app.enableCors({
-    origin: //corsOrigins
-      true,
+    origin: corsOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Status', 'Idempotency-Key'],
     credentials: true,
   });
+
+  const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 8080);
 }
